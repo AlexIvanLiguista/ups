06493b02d13bbc9ab64bb5af4d6dd957041d7b62
AlexIvanLiguista <siempreliguista_1993@hotmail.com>
2023-10-05 20:37:09 -0500

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,12 +3,15 @@ require('../database');
 const morgan=require('morgan');
 const cors = require('cors');
 const app=express();
-app.use(express.json);
+app.use(cors());
 app.use('/api/empleados',require('./routes/empleados.routes'));
+app.use(express.json);
+
+
 //settings
 app.set('puerto',process.env.PORT|| 3000);
 app.set('nombreApp','Gestión de empleados');
 //app.use(morgan('dev'));
-app.use(cors({origin: 'http://localhost:4200'}));
+
 module.exports=app;
 
